ce00bf46e64652c1a3e8694d48854dc6d29b0a67
Patrick <ruomungai@gmail.com>
2022-06-06 21:24:41 +0300

    commit tree e34f17ba12325f94f8005daa0f9dcdae24c87a3f

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feetValue){
   // Write your code here!
+  if(feetValue<=400){
+    return 'This one is on me!'
+  }
+  else if(feetValue>=2001 && feetValue<=2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if(feetValue>=2501){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+   return cityName==='NYC'?'Ok, sounds good.':'No go.';
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(thankNote){
   // Write your code here!
+  switch(thankNote){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return'Thank you.'
+    default:
+      return 'Bye.'
+  }
+
 }
